31a27f5a137b44298373bf4df5e7d30b2b85e465
FahimShahryer <56716260+FahimShahryer@users.noreply.github.com>
2023-01-27 14:54:57 +0600

    updated voice recognition

--- voice_recognition & interface.py
+++ voice_recognition & interface.py
@@ -1,32 +1,22 @@
+import random
 import speech_recognition as sr
 import pyttsx3
-
-
-r = sr.Recognizer()
+import cv2
 
 
 
-while (1):
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as mic:
+        r.adjust_for_ambient_noise(mic, duration=0.2)
+        audio = r.listen(mic)
+        MyText=""
+        try:
+            MyText = r.recognize_google(audio)
+        except sr.RequestError as e:
+            return 0
+    return MyText
 
 
-    try:
-
-        # use the microphone as source for input.
-        with sr.Microphone() as source2:
-
-
-            r.adjust_for_ambient_noise(source2, duration=0.2)
-
-            audio2 = r.listen(source2)
-
-            MyText = r.recognize_google(audio2)
-            MyText = MyText.lower()
-
-            print(MyText)
-
-
-    except sr.RequestError as e:
-        print("Could not request results; {0}".format(e))
-
-    except sr.UnknownValueError:
-        print("unknown error occurred")
+x=get_audio()
+print(x)
